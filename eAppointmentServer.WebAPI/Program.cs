60fc478d91728e503108cc3820261f85a9427dc9
elifcobann <95699826+elifcobann@users.noreply.github.com>
2026-01-12 19:01:14 +0300

    Cors Policy ve Login endpoint

--- eAppointmentServer.WebAPI/Program.cs
+++ eAppointmentServer.WebAPI/Program.cs
@@ -4,14 +4,16 @@ using eAppointmentServer.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration); 
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+//  CORS service kaydý
+builder.Services.AddCors();
 
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
@@ -20,6 +22,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//  CORS middleware (ÇOK ÖNEMLÝ: Authorization'dan ÖNCE)
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .SetIsOriginAllowed(_ => true));
 
 app.UseAuthorization();
 
