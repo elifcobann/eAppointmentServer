1143b06d1ef67de21d65fea7c61d4d1ad5168d11
elifcobann <95699826+elifcobann@users.noreply.github.com>
2026-01-21 21:26:26 +0300

    DoktorGetAll Features ve controller eklendi

---
+++ eAppointmentServer.WebAPI/Controllers/DoctorController.cs
@@ -0,0 +1,21 @@
+﻿using eAppointmentServer.Application.Features.Doctors.GetAllDoctor;
+using eAppointmentServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAppointmentServer.WebAPI.Controllers;
+
+public sealed class DoctorController : ApiController
+{
+
+    public DoctorController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetAll(GetAllDoctorQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
